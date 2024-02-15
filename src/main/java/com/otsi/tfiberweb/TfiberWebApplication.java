d34bad2fbb175fcff77a90a78fa659fd6341e970
Raj Kumar <64770654+rajthummeda@users.noreply.github.com>
2024-02-15 16:06:33 +0530

    War update

--- src/main/java/com/otsi/tfiberweb/TfiberWebApplication.java
+++ src/main/java/com/otsi/tfiberweb/TfiberWebApplication.java
@@ -2,9 +2,10 @@ package com.otsi.tfiberweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TfiberWebApplication {
+public class TfiberWebApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TfiberWebApplication.class, args);
